89f3a9e188d2a4cd5324f9c86571bb773cadaa93
natekred981 <nkredent@uwaterloo.ca>
2021-07-02 17:25:25 -0400

    organization

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def predict():
+    return render_template('home.html')
+if __name__ == '__main__':
+   app.run(debug=False)
